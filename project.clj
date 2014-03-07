987199223f773bda995b2f4ce27934e583e30b21
Gary Fredericks <garyf@groupon.com>
2014-03-07 15:49:40 +0000

    lein-release stuff

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject com.gfredericks/repl-utils "0.1.0"
   :description "Gary Fredericks' repl utils."
+  :url "https://github.com/fredericksgary/repl-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :lein-release {:deploy-via :clojars})
