405c953ab711a6bc573f170bc820f7ab20fb7ee7
Gary Fredericks <garyf@groupon.com>
2014-03-07 15:29:28 +0000

    a bg macro

--- src/com/gfredericks/repl.clj
+++ src/com/gfredericks/repl.clj
@@ -1,2 +1,18 @@
 (ns com.gfredericks.repl
   "My repl utilities.")
+
+(defn run-and-report
+  [func]
+  (future (try (time (func))
+               (println "Finished!")
+               (catch Throwable t
+                 (println "Error!")
+                 (throw t)))))
+
+(defmacro bg
+  "Runs code in a future, defs the future to a var, and prints a
+  message when it finishes."
+  [& body]
+  (let [sym (symbol (format "r%02d" (rand-int 100)))]
+    `(do (def ~sym (run-and-report (fn [] ~@body)))
+         (println "Defined future as" '~sym))))
