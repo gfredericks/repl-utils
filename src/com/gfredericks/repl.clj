c49f3b26711e08683a5165de5e4a683ecc1e63f6
Gary Fredericks <garyf@groupon.com>
2014-03-07 16:39:57 +0000

    Better bg, bootstrappability

--- src/com/gfredericks/repl.clj
+++ src/com/gfredericks/repl.clj
@@ -1,18 +1,32 @@
 (ns com.gfredericks.repl
   "My repl utilities.")
 
+(defonce bg-id-counter (atom -1))
+
 (defn run-and-report
-  [func]
-  (future (try (time (func))
-               (println "Finished!")
+  [var var-name func]
+  (future (try (let [res (time (func))]
+                 (println "Background job" var-name "finished")
+                 (alter-var-root var (constantly res)))
                (catch Throwable t
-                 (println "Error!")
-                 (throw t)))))
+                 (println (str "Error in background job " var-name "!"))
+                 (println t)
+                 (alter-var-root var (constantly t))))))
 
 (defmacro bg
   "Runs code in a future, defs the future to a var, and prints a
   message when it finishes."
   [& body]
-  (let [sym (symbol (format "r%02d" (rand-int 100)))]
-    `(do (def ~sym (run-and-report (fn [] ~@body)))
-         (println "Defined future as" '~sym))))
+  (let [sym (symbol (str "bg" (swap! bg-id-counter inc)))]
+    `(do (println "Starting background task" '~sym)
+         (def ~sym (run-and-report (var ~sym) '~sym (fn [] ~@body))))))
+
+(defn bootstrap-ns
+  []
+  (require '[clojure.repl :refer :all]
+           '[com.gfredericks.repl :refer :all]))
+
+(binding [*ns* (the-ns 'clojure.core)]
+  (eval '(def repl
+           "Pulls repl-utility stuff into this namespace."
+           com.gfredericks.repl/bootstrap-ns)))
