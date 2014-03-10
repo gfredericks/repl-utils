dc18621fadba25f78a2f73978381a5faf39017c3
Gary Fredericks <fredericksgary@gmail.com>
2014-03-10 15:45:10 -0500

    lein-release plugin: preparing 0.1.7 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/repl-utils "0.1.7-SNAPSHOT"
+(defproject com.gfredericks/repl-utils "0.1.7"
   :description "Gary Fredericks' repl utils."
   :url "https://github.com/fredericksgary/repl-utils"
   :license {:name "Eclipse Public License"
