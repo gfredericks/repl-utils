5c1fb715b31d9126f1fcf05d36eba5f5d44b77b6
Gary Fredericks <fredericksgary@gmail.com>
2014-04-01 10:33:22 -0500

    lein-release plugin: bumped version from 0.1.10 to 0.1.11-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/repl-utils "0.1.10"
+(defproject com.gfredericks/repl-utils "0.1.11-SNAPSHOT"
   :description "Gary Fredericks' repl utils."
   :url "https://github.com/fredericksgary/repl-utils"
   :license {:name "Eclipse Public License"
