e1d1bcc26d4322f82b08fd167535c2a8ddc4093a
Gary Fredericks <fredericksgary@gmail.com>
2014-06-12 17:05:47 -0500

    Add debug-repl stuff

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
+                 [com.gfredericks/debug-repl "0.0.1"]
                  [org.clojure/clojure "1.6.0"]
                  [potemkin "0.3.4"]]
   :lein-release {:deploy-via :clojars})
--- src/user.clj
+++ src/user.clj
@@ -1,8 +1,10 @@
 (ns .
   (:require [clojure.repl]
+            [com.gfredericks.debug-repl]
             [com.gfredericks.repl]
             [potemkin :refer [import-vars]]))
 
 (import-vars
  [clojure.repl apropos doc pst source]
+ [com.gfredericks.debug-repl break! unbreak!]
  [com.gfredericks.repl add-dep dir pprint pp bg])
