433fdd8fef648ffb052785c94a04bfc5e9d44ff5
Gary Fredericks <fredericksgary@gmail.com>
2014-06-12 16:15:04 -0500

    Start using a short namespace instead of referring things everywhere

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
-                 [org.clojure/clojure "1.5.1"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [potemkin "0.3.4"]]
   :lein-release {:deploy-via :clojars})
--- src/user.clj
+++ src/user.clj
@@ -1,3 +1,8 @@
-(ns user
-  (:require [clojure.repl :refer :all :exclude [dir]]
-            [com.gfredericks.repl :refer :all]))
+(ns .
+  (:require [clojure.repl]
+            [com.gfredericks.repl]
+            [potemkin :refer [import-vars]]))
+
+(import-vars
+ [clojure.repl apropos doc pst source]
+ [com.gfredericks.repl add-dep dir pprint pp bg])
