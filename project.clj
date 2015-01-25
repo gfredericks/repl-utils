7a4cab3699f33124698130fa11820c809cb568df
Gary Fredericks <fredericksgary@gmail.com>
2015-01-24 20:14:11 -0600

    Macroexpansion

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
-                 [org.clojure/clojure "1.6.0"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer.jvm "0.6.5"]]
   :deploy-repositories [["releases" :clojars]])
