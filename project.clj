f26f817a3fe011b66f593b5319db195e808e87d0
Gary Fredericks <fredericksgary@gmail.com>
2015-03-19 15:07:52 -0500

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/repl-utils "0.2.3"
+(defproject com.gfredericks/repl-utils "0.2.4-SNAPSHOT"
   :description "Gary Fredericks' repl utils."
   :url "https://github.com/fredericksgary/repl-utils"
   :license {:name "Eclipse Public License"
