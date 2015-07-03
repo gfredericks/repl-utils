99a8b4b2d8037a6db1aeb1af139dc8e18e5ae5a7
Gary Fredericks <fredericksgary@gmail.com>
2015-07-03 09:47:55 -0500

    The java.classpath dep is necessary for run-all-tests

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
                  [org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.classpath "0.2.2"]
                  [org.clojure/tools.analyzer.jvm "0.6.5"]
                  [org.clojure/tools.namespace "0.2.10"]]
   :deploy-repositories [["releases" :clojars]])
