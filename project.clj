d4ffb8df126ae8a122c0a81d98967f63f87c4af3
Gary Fredericks <fredericksgary@gmail.com>
2017-03-21 06:18:23 -0500

    Upgrade tools.analyzer.jvm

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[com.cemerick/pomegranate "0.3.0"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.2"]
-                 [org.clojure/tools.analyzer.jvm "0.6.5"]
+                 [org.clojure/tools.analyzer.jvm "0.7.0"]
                  [org.clojure/tools.namespace "0.2.10"]]
   :deploy-repositories [["releases" :clojars]])
