4a7408fd622e54a9553fa5317e48e7f3d15af247
Gary Fredericks <fredericksgary@gmail.com>
2017-12-05 07:16:20 -0600

    Version 0.2.15

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Some things I made to help my Clojure repl and you can too.
 ## Obtention
 
 ```
-[com.gfredericks/repl-utils "0.2.14"]
+[com.gfredericks/repl-utils "0.2.15"]
 ```
 
 ## The Good Parts
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/repl-utils "0.2.15-SNAPSHOT"
+(defproject com.gfredericks/repl-utils "0.2.15"
   :description "Gary Fredericks' repl utils."
   :url "https://github.com/fredericksgary/repl-utils"
   :license {:name "Eclipse Public License"
