cd997e27a784bc048d031945de5b4e99e09b5511
Gary Fredericks <fredericksgary@gmail.com>
2019-04-02 20:15:37 -0500

    Upgrade dependencies
    I think the older classpath dep is incompatible
    with newer JVMs.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.classpath "0.2.2"]
-                 [org.clojure/tools.analyzer.jvm "0.7.0"]
+                 [org.clojure/java.classpath "0.3.0"]
+                 [org.clojure/tools.analyzer.jvm "0.7.2"]
                  [org.clojure/tools.namespace "0.2.10"]]
   :deploy-repositories [["releases" :clojars]])
